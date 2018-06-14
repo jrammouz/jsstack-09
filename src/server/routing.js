7a2e2ab98f1cd2cf90015c28a4e3e943fbf63399
Julie-Ann Rammouz <julieannrammouz@gmail.com>
2018-06-14 14:36:14 +0300

    feat: src folder

---
+++ src/server/routing.js
@@ -0,0 +1,50 @@
+// @flow
+
+import {
+  homePage,
+  helloPage,
+  helloAsyncPage,
+  helloEndpoint,
+} from './controller'
+
+import {
+  HOME_PAGE_ROUTE,
+  HELLO_PAGE_ROUTE,
+  HELLO_ASYNC_PAGE_ROUTE,
+  helloEndpointRoute,
+} from '../shared/routes'
+
+import renderApp from './render-app'
+
+export default (app: Object) => {
+  app.get(HOME_PAGE_ROUTE, (request, response) => {
+    response.send(renderApp(request.url, homePage()))
+  })
+
+  app.get(HELLO_PAGE_ROUTE, (request, response) => {
+    response.send(renderApp(request.url, helloPage()))
+  })
+
+  app.get(HELLO_ASYNC_PAGE_ROUTE, (request, response) => {
+    response.send(renderApp(request.url, helloAsyncPage()))
+  })
+
+  app.get(helloEndpointRoute(), (request, response) => {
+    response.json(helloEndpoint(request.params.num))
+  })
+
+  app.get('/500', () => {
+    throw Error('Fake Internal Server Error')
+  })
+
+  app.get('*', (request, response) => {
+    response.status(404).send(renderApp(request.url))
+  })
+
+  // eslint-disable-next-line no-unused-vars
+  app.use((err, request, response, next) => {
+    // eslint-disable-next-line no-console
+    console.error(err.stack)
+    response.status(404).send('Something went wrong!')
+  })
+}
